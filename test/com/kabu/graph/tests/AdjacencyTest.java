489b96ad5b45ba9ba2e70d5322b3b8073fcb2ec8
Kabushiki <jerome.badt@gmail.com>
2014-11-27 16:53:30 +0100

    Use AssertJ instead of Hamcrest

--- test/com/kabu/graph/tests/AdjacencyTest.java
+++ test/com/kabu/graph/tests/AdjacencyTest.java
@@ -1,11 +1,8 @@
 package com.kabu.graph.tests;
 
-import static org.junit.Assert.*;
-
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import com.kabu.graph.Edge;
 import com.kabu.graph.Graph;
 import com.kabu.graph.Vertex;
 
@@ -25,28 +22,28 @@ public class AdjacencyTest {
 		Vertex v3 = g.createVertex("Node 3");
 		Vertex v4 = g.createVertex("Node 4");
 
-		Edge e1 = g.connect(v1, v2, "N1->N2");
-		Edge e2 = g.connect(v1, v3, "N1->N3");
-		Edge e3 = g.connect(v1, v4, "N1->N4");
-		Edge e4 = g.connect(v2, v4, "N2->N4");
-		Edge e5 = g.connect(v3, v4, "N3->N4");
-		Edge e6 = g.connect(v4, v4, "N4->N4");
-		Edge e7 = g.connect(v4, v1, "N4->N1");
+		g.connect(v1, v2, "N1->N2");
+		g.connect(v1, v3, "N1->N3");
+		g.connect(v1, v4, "N1->N4");
+		g.connect(v2, v4, "N2->N4");
+		g.connect(v3, v4, "N3->N4");
+		g.connect(v4, v4, "N4->N4");
+		g.connect(v4, v1, "N4->N1");
 
-		assertEquals(v1.getLabel(), "Node 1");
-		assertEquals(e1.getLabel(), "N1->N2");
+		assertThat(g.getVertices().get(0).getLabel()).isEqualTo("Node 1");
+		assertThat(g.getAllEdges().get(0).getLabel()).isEqualTo("N1->N2");
 
-		assertThat(v1.getNeighbours(), CoreMatchers.hasItems(v2, v3, v4));
-		assertThat(v2.getNeighbours(), CoreMatchers.hasItems(v4));
-		assertThat(v3.getNeighbours(), CoreMatchers.hasItems(v4));
-		assertThat(v4.getNeighbours(), CoreMatchers.hasItems(v4, v1));
+		assertThat(v1.getNeighbours()).containsExactly(v2, v3, v4);
+		assertThat(v2.getNeighbours()).containsExactly(v4);
+		assertThat(v3.getNeighbours()).containsExactly(v4);
+		assertThat(v4.getNeighbours()).containsExactly(v4, v1);
 
-		Convert2AdjacencyMatrix a = new Convert2AdjacencyMatrix(g);
-		a.execute();
+		Convert2AdjacencyMatrix algorithm = new Convert2AdjacencyMatrix(g);
+		algorithm.execute();
 
-		assertThat(a.getResult(), CoreMatchers.is(new int[][] {
-						{ 0, 1, 1, 1 }, { 0, 0, 0, 1 },
-						{ 0, 0, 0, 1 }, { 1, 0, 0, 1 }}));
+		assertThat(algorithm.getResult()).isEqualTo(
+				new int[][] { { 0, 1, 1, 1 }, { 0, 0, 0, 1 }, { 0, 0, 0, 1 },
+						{ 1, 0, 0, 1 } });
 	}
 
 }
--- test/com/kabu/graph/tests/WeightTest.java
+++ test/com/kabu/graph/tests/WeightTest.java
@@ -1,10 +1,9 @@
 package com.kabu.graph.tests;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
-import com.kabu.graph.Edge;
 import com.kabu.graph.Graph;
 import com.kabu.graph.Vertex;
 
@@ -12,30 +11,35 @@ import com.kabu.graph.Vertex;
  * Einfacher Funktionstest zur Kantenbewertung von Graphen.
  */
 public class WeightTest {
-	
+
 	@Test
 	public void test() {
-        Graph g = new Graph();
+		Graph g = new Graph();
 
-        Vertex v1 = g.createVertex("Node 1");
-        Vertex v2 = g.createVertex("Node 2");
-        Vertex v3 = g.createVertex("Node 3");
-        Vertex v4 = g.createVertex("Node 4");
+		Vertex v1 = g.createVertex("Node 1");
+		Vertex v2 = g.createVertex("Node 2");
+		Vertex v3 = g.createVertex("Node 3");
+		Vertex v4 = g.createVertex("Node 4");
 
-        Edge e1 = g.connect(v1, v2, "N1->N2", 3);
-        Edge e2 = g.connect(v1, v3, "N1->N3", 1);
-        Edge e3 = g.connect(v1, v4, "N1->N4", 5);
-        Edge e4 = g.connect(v2, v4, "N2->N4", 8);
-        Edge e5 = g.connect(v3, v4, "N3->N4", 0.3);
+		g.connect(v1, v2, "N1->N2", 3);
+		g.connect(v1, v3, "N1->N3", 1);
+		g.connect(v1, v4, "N1->N4", 5);
+		g.connect(v2, v4, "N2->N4", 8);
+		g.connect(v3, v4, "N3->N4", 0.3);
 
-        System.out.println(g.toString());
-        
-        final double DELTA = 1e-15;
-        
-        assertEquals(g.getAllEdges().get(0).getWeight(), 3, DELTA);
-        assertEquals(g.getAllEdges().get(1).getWeight(), 1, DELTA);
-        assertEquals(g.getAllEdges().get(2).getWeight(), 5, DELTA);
-        assertEquals(g.getAllEdges().get(3).getWeight(), 8, DELTA);
-        assertEquals(g.getAllEdges().get(4).getWeight(), 0.3, DELTA);
-    }
+		System.out.println(g.toString());
+
+		final double DELTA = 1e-15;
+
+		assertThat(g.getAllEdges().get(0).getWeight()).isCloseTo(3.0,
+				within(DELTA));
+		assertThat(g.getAllEdges().get(1).getWeight()).isCloseTo(1.0,
+				within(DELTA));
+		assertThat(g.getAllEdges().get(2).getWeight()).isCloseTo(5.0,
+				within(DELTA));
+		assertThat(g.getAllEdges().get(3).getWeight()).isCloseTo(8.0,
+				within(DELTA));
+		assertThat(g.getAllEdges().get(4).getWeight()).isCloseTo(0.3,
+				within(DELTA));
+	}
 }
